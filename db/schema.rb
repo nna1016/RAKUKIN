31f0e8138eb8585f6fdc3e3e16a636881170dfc4
Nagi <n.ishizuka@becalm.jp>
2022-07-13 15:22:01 +0900

    アカウント認証まわり・出勤簿テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_162836) do
+ActiveRecord::Schema.define(version: 2022_07_13_053331) do
+
+  create_table "attendances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "updater"
+    t.datetime "in"
+    t.datetime "out"
+    t.integer "benefit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
