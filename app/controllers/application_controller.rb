2451dd46ba232ee0260289e4762cf932b4dec3e1
Nagi <n.ishizuka@becalm.jp>
2022-07-20 16:19:32 +0900

    氏名の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-      added_attrs = [ :email, :name, :department, :wage, :password, :password_confirmation ]
-      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+      devise_parameter_sanitizer.permit (:sign_up, keys: [ :email, :name, :department, :wage, :password, :password_confirmation ])
+      devise_parameter_sanitizer.permit (:account_update, keys: [ :email, :name, :department, :wage, :password, :password_confirmation ])
+      devise_parameter_sanitizer.permit (:sign_in, keys: [ :email, :name, :department, :wage, :password, :password_confirmation ])
     end
 end
