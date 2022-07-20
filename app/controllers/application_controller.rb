9706e6e7b6916d1353a4b635995e7cd825ba13f7
Nagi <n.ishizuka@becalm.jp>
2022-07-20 16:28:27 +0900

    パラメータ適応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit (:sign_up, keys: [ :email, :name, :department, :wage, :password, :password_confirmation ])
-      devise_parameter_sanitizer.permit (:account_update, keys: [ :email, :name, :department, :wage, :password, :password_confirmation ])
-      devise_parameter_sanitizer.permit (:sign_in, keys: [ :email, :name, :department, :wage, :password, :password_confirmation ])
+      added_attrs = [ :email, :name, :department, :wage, :password, :password_confirmation ]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 end
